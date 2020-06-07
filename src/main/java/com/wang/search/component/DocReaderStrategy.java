35b8afb4625cfe328ecb4e1892e48a9ba644d129
wangjunhao <wangjunhao>
2020-06-07 20:00:42 +0800

    PDF读取类

--- src/main/java/com/wang/search/component/DocReaderStrategy.java
+++ src/main/java/com/wang/search/component/DocReaderStrategy.java
@@ -4,6 +4,7 @@ import com.wang.search.constant.DocFormatConstant;
 import com.wang.search.constant.ReturnCode;
 import com.wang.search.core.DocumentEntity;
 import com.wang.search.exception.DocException;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.InputStream;
@@ -16,18 +17,15 @@ import java.io.InputStream;
  * @date 2020/6/6 12:25
  * @since JDK 1.8
  */
-public class DocReaderStrategy implements DocumentReaderStrategy{
+@Component(value = "docReader")
+public class DocReaderStrategy implements DocumentReaderStrategy {
     @Override
-    public DocumentEntity read(byte[] file,String fileName) {
+    public DocumentEntity read(byte[] file, String fileName) {
         String suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length());
-
-        if (null == file) {
-            throw new DocException(ReturnCode.DOCUMENT_NOT_FOUND);
-        }
         if (DocFormatConstant.WORD_2003_SUFFIX.equals(suffix)) {
-
+            return new DocumentEntity().setContent("doc");
         } else if (DocFormatConstant.WORD_2007_SUFFIX.equals(suffix)) {
-
+            return new DocumentEntity().setContent("doc");
         }
         throw new DocException(ReturnCode.DOCUMENT_FORMAT_ERROR);
     }
--- src/main/java/com/wang/search/component/PdfReaderStrategy.java
+++ src/main/java/com/wang/search/component/PdfReaderStrategy.java
@@ -3,20 +3,18 @@ package com.wang.search.component;
 import com.wang.search.core.DocumentEntity;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-
 /**
- * Excel读取策略
+ * PDF读取
  *
  * @author wangjunhao
  * @version 1.0
- * @date 2020/6/6 12:46
+ * @date 2020/6/7 14:47
  * @since JDK 1.8
  */
-@Component(value = "excelReader")
-public class ExcelReaderStrategy implements DocumentReaderStrategy {
+@Component(value = "pdfReader")
+public class PdfReaderStrategy implements DocumentReaderStrategy{
     @Override
-    public DocumentEntity read(byte[] file,String fileName) {
+    public DocumentEntity read(byte[] file, String fileName) {
         return null;
     }
 }
